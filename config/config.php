2814d256c2ee22ceb9c3d68eb6f3a248c622cbf0
Darwin Besorio <87165357+Dewei16@users.noreply.github.com>
2023-03-15 08:04:10 +0800

    Add files via upload
    index

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    $dbuser="root";
+    $dbpass="";
+    $host="localhost";
+    $db="liblio_ils";
+    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+<footer id="footer">
+         &copy; <?php echo date('Y');?>  LiBlio Integrated Library System. Powered By 
+        <a target="_blank" href="https://www.facebook.com/LiBlio.Info">LiBLio.Info</a>, All rights reserved.
+</footer>
